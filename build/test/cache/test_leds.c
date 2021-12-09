e89ddb51db599338ece436250a98f93ec1ea2273
Junior Monroy <juniordaniel1988@gmail.com>
2021-12-08 19:33:16 -0800

    Inicio

---
+++ build/test/cache/test_leds.c
@@ -0,0 +1,10 @@
+#include "/var/lib/gems/2.5.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h"
+
+
+void test_todos_los_leds_inician_apagados(void) {
+
+
+
+  UnityFail( (("Arrancamos")), (UNITY_UINT)(5));
+
+}
---
+++ test/test_leds.c
@@ -0,0 +1,19 @@
+/*
+ *Despues de la configuracion todos los leds deben quedar apagados
+ *Prender un led cualquiera
+ *Apagar un led cualquiera
+ *Prender y apagar algunos leds
+ *Prender todos los leds juntos
+ *Apagar todos los leds juntos
+ *Consultar el estado de un leds apagado
+ *Consultar el estado de un led encendido
+ *Revisar los valores limites de los parametros
+ *Probar valores invalidos para los parametros
+ */
+
+#include "unity.h"
+
+void test_todos_los_leds_inician_apagados(void) {
+
+  TEST_FAIL_MESSAGE("Arrancamos");
+}
